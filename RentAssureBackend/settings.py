51ac47268597ee83e42c8f2e8efc79236709747c
rudresh gupta <rudreshgpt7@gmail.com>
2021-07-01 21:16:28 +0530

    minor change

--- RentAssureBackend/settings.py
+++ RentAssureBackend/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '%pqa&19x$##0o5nglb@h3-+k67t^!==!dw0&buose!m-7m65le'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '%pqa&19x$##0o5nglb@h3-+k67t^!==!dw0&buose!m-7m65le'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
